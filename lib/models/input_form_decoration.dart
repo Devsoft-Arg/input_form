e538752bc9d3011bb3799c05942f562559b23256
Gabriel Casares <luisgabrielcasares@gmail.com>
2022-08-03 18:05:24 -0300

    feat: add notValidErrorText

--- lib/models/input_form_decoration.dart
+++ lib/models/input_form_decoration.dart
@@ -6,19 +6,19 @@ class InputFormDecoration {
     this.borderColor = Colors.grey,
     this.selectedBorderColor = Colors.blue,
     this.errorBorderColor = Colors.red,
-    this.backgroundColor = Colors.transparent,
     this.iconColor = Colors.grey,
     this.selectedIconColor = Colors.grey,
     this.buttonBackgroundColor = Colors.blue,
     this.hintColor = Colors.grey,
+    this.backgroundColor,
     this.nullErrorText,
+    this.notValidErrorText,
     this.titleStyle,
     this.hintStyle,
     this.textStyle,
     BorderRadius? borderRadius,
   }) : borderRadius = borderRadius ?? BorderRadius.circular(4);
 
-  final Color backgroundColor;
   final Color borderColor;
   final Color selectedBorderColor;
   final Color errorBorderColor;
@@ -27,7 +27,9 @@ class InputFormDecoration {
   final Color buttonBackgroundColor;
   final Color hintColor;
   final BorderRadius borderRadius;
+  final Color? backgroundColor;
   final String? nullErrorText;
+  final String? notValidErrorText;
   final TextStyle? titleStyle;
   final TextStyle? hintStyle;
   final TextStyle? textStyle;
