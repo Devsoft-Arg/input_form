6564f7eb83e6169523929c4fde5bd9017dca8255
Gabriel Casares <luisgabrielcasares@gmail.com>
2022-09-12 20:01:39 -0300

    feat: add dependency export

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -1,5 +1,8 @@
 library input_form;
 
+export 'package:intl/date_symbol_data_local.dart';
+export 'package:intl/intl.dart';
+
 export './models/models.dart';
 export './providers/providers.dart';
 export './widgets/widgets.dart';
