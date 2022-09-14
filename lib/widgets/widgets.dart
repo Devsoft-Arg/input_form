6e13f46e55905611b7f2de2f7fa242484a56e08d
Gabriel Casares <luisgabrielcasares@gmail.com>
2022-09-14 16:14:05 -0300

    fix: add input-field export

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,6 +2,7 @@ export './date_input_field.dart';
 export './dropdown_input_field.dart';
 export './file_input_field.dart';
 export './image_input_field.dart';
+export './input_field.dart';
 export './input_form.dart';
 export './text_button.dart';
 export './text_input_field.dart';
